67802760829afab8eba802af206c8c43d4739c59
woong <koohw0109@gmail.com>
2023-08-11 14:20:41 +0900

    jsondata

---
+++ lib/controllers/question_paper/data_uploader.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:study_app/models/question_paper_model.dart';
+
+class DataUploader extends GetxController {
+  @override
+  void onReady() {
+    uploadData();
+    super.onReady();
+  }
+
+  Future<void> uploadData() async {
+    final manifestContent = await DefaultAssetBundle.of(Get.context!)
+        .loadString("AssetManifest.json");
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+    /** load json fle and print path */
+    final papersInAssets = manifestMap.keys
+        .where((path) =>
+            path.startsWith("assets/DB/papers") && path.contains(".json"))
+        .toList();
+    List<QuestionPaperModel> questionPapers = [];
+    for (var paper in papersInAssets) {
+      String stringPaperContent = await rootBundle.loadString(paper);
+      questionPapers
+          .add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
+    }
+    print("Items number ${questionPapers.length}");
+  }
+}
