675320fb1310dfb0e91e1c3572cae248363cacdf
woong <koohw0109@gmail.com>
2023-08-13 19:07:34 +0900

    overview

--- lib/screens/question/test_overview_screen.dart
+++ lib/screens/question/test_overview_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:get/get.dart';
+import 'package:study/configs/themes/custom_text_styles.dart';
+import 'package:study/configs/themes/ui_parameters.dart';
 import 'package:study/controllers/question_paper/questions_controller.dart';
 import 'package:study/widgets/common/background_decoration.dart';
 import 'package:study/widgets/common/custom_app_bar.dart';
+import 'package:study/widgets/content_area.dart';
+import 'package:study/widgets/questions/countdown_timer.dart';
 
 class TestOverviewScreen extends GetView<QuestionsController> {
   const TestOverviewScreen({super.key});
@@ -11,16 +15,60 @@ class TestOverviewScreen extends GetView<QuestionsController> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: CustomAppBar(
-        title: "Test over",
+        title: controller.completedTest,
       ),
       body: BackgroundDecoration(
-        child: Center(
-          child: Text("asd"),
-        ),
-      ),
+          child: Column(
+        children: [
+          Expanded(
+            child: ContentArea(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CountdownTimer(
+                        color: UIParameters.isDarkMode()
+                            ? Theme.of(context).textTheme.bodyLarge!.color
+                            : Theme.of(context).primaryColor,
+                        time: '',
+                      ),
+                      Obx(
+                        () => Text(
+                          '${controller.time} Remaining',
+                          style: countdownTimerTS(),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Expanded(
+                    child: GridView.builder(
+                      itemCount: controller.allQuestions.length,
+                      shrinkWrap: true,
+                      physics: const BouncingScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: Get.width ~/ 75,
+                        childAspectRatio: 1,
+                        crossAxisSpacing: 8,
+                        mainAxisSpacing: 8,
+                      ),
+                      itemBuilder: (_, index) {
+                        return Text(
+                          '${controller.allQuestions[index].selectedAnswer != null}',
+                          style: const TextStyle(fontSize: 20),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
