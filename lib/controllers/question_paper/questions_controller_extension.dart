746a3cb6af51247f2f9b4c42a0af2dc90ea9892e
woong <koohw0109@gmail.com>
2023-08-13 21:09:22 +0900

    answer gridview

--- lib/controllers/question_paper/questions_controller_extension.dart
+++ lib/controllers/question_paper/questions_controller_extension.dart
@@ -5,4 +5,17 @@ extension QuestionsControllerExtension on QuestionsController {
       .where((element) => element.selectedAnswer == element.correctAnswer)
       .toList()
       .length;
+
+  String get correctAnswerQuestions {
+    return '$correctQuestionCount out of ${allQuestions.length} are correct';
+  }
+
+  String get points {
+    var points = (correctQuestionCount / allQuestions.length) *
+        100 *
+        (questionPaperModel.timeSeconds - remainSeconds) /
+        questionPaperModel.timeSeconds *
+        100;
+    return points.toStringAsFixed(2);
+  }
 }
--- lib/screens/question/result_screen.dart
+++ lib/screens/question/result_screen.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:study/configs/themes/custom_text_styles.dart';
 import 'package:study/controllers/question_paper/questions_controller_extension.dart';
+import 'package:study/widgets/common/background_decoration.dart';
+import 'package:study/widgets/common/custom_app_bar.dart';
+import 'package:study/widgets/content_area.dart';
+import 'package:study/widgets/questions/answer_card.dart';
+import 'package:study/widgets/questions/question_number_card.dart';
 
 import '../../controllers/question_paper/questions_controller.dart';
 
@@ -11,10 +18,81 @@ class ResultScreen extends GetView<QuestionsController> {
 
   @override
   Widget build(BuildContext context) {
+    Color textColor =
+        Get.isDarkMode ? Colors.white : Theme.of(context).primaryColor;
     return Scaffold(
-        body: Center(
-      child: Text(
-        controller.correctQuestionCount.toString(),
+        body: BackgroundDecoration(
+      child: Column(
+        children: [
+          CustomAppBar(
+            leading: const SizedBox(height: 80),
+            title: controller.correctAnswerQuestions,
+          ),
+          Expanded(
+            child: ContentArea(
+              child: Column(
+                children: [
+                  SvgPicture.asset("assets/images/bulb.svg"),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 20,
+                      bottom: 5,
+                    ),
+                    child: Text(
+                      "Congratulations",
+                      style: headerText.copyWith(color: textColor),
+                    ),
+                  ),
+                  Text(
+                    "You have ${controller.points} points",
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  const Text(
+                    "Tap below question numbers to view correct answers",
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 25),
+                  Expanded(
+                    child: GridView.builder(
+                      itemCount: controller.allQuestions.length,
+                      shrinkWrap: true,
+                      physics: const BouncingScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: Get.width ~/ 75,
+                        childAspectRatio: 1,
+                        crossAxisSpacing: 8,
+                        mainAxisSpacing: 8,
+                      ),
+                      itemBuilder: (_, index) {
+                        final question = controller.allQuestions[index];
+                        AnswerStatus status = AnswerStatus.notanswered;
+                        final selectedAnswer = question.selectedAnswer;
+                        final correctAnswer = question.correctAnswer;
+                        if (selectedAnswer == correctAnswer) {
+                          status = AnswerStatus.correct;
+                        } else if (question.selectedAnswer == null) {
+                          status = AnswerStatus.wrong;
+                        } else {
+                          status = AnswerStatus.wrong;
+                        }
+                        return QuestionNumberCard(
+                          index: index + 1,
+                          status: status,
+                          onTap: () {
+                            controller.jumpToQuestion(index, isGoBack: false);
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     ));
   }
